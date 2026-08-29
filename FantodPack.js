4d46af96176452adecdea444b26d76965a207da4
kayla-kubicke <161649341+kayla-kubicke@users.noreply.github.com>
2026-08-28 18:08:38 -0700

    The Basics

---
+++ FantodPack.js
@@ -0,0 +1,98 @@
+// Less code, more creative writing; sadly, I have both afflictions.
+
+// Just an adorable deck.
+// This is my interpretation of the deck; the pack comes with a booklet that one can use.
+
+// Unlike most decks, I read Fantod cards the same regardless if they are upright or reversed.
+// One could assume a slightly more negative connotation but, given the deck is quite dark already,
+// emphasising this is unnecessary.
+
+
+// Enjoy the New Year tradition beloved by dark weirdos.
+// Obviously:
+// Just for fun. I'm not a psychic, just use them for meditative and entertainment purposes.
+
+export class FantodPack {
+    static FantodCard = {
+        TheLadder: {
+            name: 'The Ladder',
+            meaning: '',
+        },
+        TheChild: {
+            name: 'The Child',
+            meaning: '',
+        },
+        TheLimb: {
+            name: 'The Limb',
+            meaning: '',
+        },
+        TheYellowBird: {
+            name: 'The Yellow Bird',
+            meaning: '',
+        },
+        TheStones: {
+            name: 'The Stones',
+            meaning: '',
+        },
+        TheEffigy: {
+            name: 'The Effigy',
+            meaning: '',
+        },
+        TheInsects: {
+            name: 'The Insects',
+            meaning: '',
+        },
+        ThePlant: {
+            name: 'The Plant',
+            meaning: '',
+        },
+        TheWaltzingMouse: {
+            name: 'The Waltzing Mouse',
+            meaning: '',
+        },
+        TheUrn: {
+            name: 'The Urn',
+            meaning: '',
+        },
+        TheFeather: {
+            name: 'The Feather',
+            meaning: '',
+        },
+        TheBundle: {
+            name: 'The Bundle',
+            meaning: '',
+        },
+        TheSea: {
+            name: 'The Sea',
+            meaning: '',
+        },
+        TheEcorche: {
+            name: 'The Ecorche',
+            meaning: '',
+        },
+        TheBottle: {
+            name: 'The Bottle',
+            meaning: '',
+        },
+        TheBurningHead: {
+            name: 'The Burning Head',
+            meaning: '',
+        },
+        TheBlueDog: {
+            name: 'The Blue Dog',
+            meaning: '',
+        },
+        TheAncestor: {
+            name: 'The Ancestor',
+            meaning: '',
+        },
+        TheTunnel: {
+            name: 'The Tunnel',
+            meaning: '',
+        },
+        TheBlackDoll: {
+            name: 'The Black Doll',
+            meaning: '',
+        },
+    }
+}
